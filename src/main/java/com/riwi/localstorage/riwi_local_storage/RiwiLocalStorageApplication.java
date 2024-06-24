b87ed4402c4266d2bbc5b01e64be493359e93543
ralexale <ralexale@gmail.com>
2024-06-24 13:10:18 -0500

    first commit with inital config

---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/RiwiLocalStorageApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.localstorage.riwi_local_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiLocalStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiLocalStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/localstorage/riwi_local_storage/RiwiLocalStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.localstorage.riwi_local_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiLocalStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
