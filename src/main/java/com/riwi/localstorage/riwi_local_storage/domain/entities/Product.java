aa5b2096a90f6dbf0061746678b49ff927eb8500
Alipser <Romarioajb@outlook.com>
2024-06-25 10:55:14 -0500

    task-32-Created_Product_Entity

---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/domain/entities/Product.java
@@ -0,0 +1,49 @@
+package com.riwi.localstorage.riwi_local_storage.domain.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @Column(name = "barcode", nullable = false)
+    private String barcode;
+
+    @Column(name = "selling_price", nullable = false)
+    private Double sellingPrice;
+
+    @Column(name = "buying_price", nullable = true)
+    private Double buyingPrice;
+
+    @Column(name = "category_id", nullable = true)
+    private String categoryId;
+    
+}
+
