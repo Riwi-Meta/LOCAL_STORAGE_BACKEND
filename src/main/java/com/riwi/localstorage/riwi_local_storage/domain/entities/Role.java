bb99141651098a0df5c8df633ceed78e462f2e22
lisSharik <lissharik@gmail.com>
2024-06-25 09:53:35 -0500

    task-18/Create_Membership_entity

---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/domain/entities/Role.java
@@ -0,0 +1,33 @@
+package com.riwi.localstorage.riwi_local_storage.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", nullable = true)
+    @Lob
+    private String description;
+}
