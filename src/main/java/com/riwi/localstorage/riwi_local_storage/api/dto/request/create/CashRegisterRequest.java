2cc6a258caaa6c1b6c3a5b027b6cb8ca7ccda674
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-06-26 08:41:41 -0500

    fix warning

--- src/main/java/com/riwi/localstorage/riwi_local_storage/api/dto/request/create/CashRegisterRequest.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/api/dto/request/create/CashRegisterRequest.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
