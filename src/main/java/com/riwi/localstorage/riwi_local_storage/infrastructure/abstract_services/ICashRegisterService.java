b2269080714cf16410fb19d718d9319abcc0b1cb
danielsm <danielsanchezm92@gmail.com>
2024-06-26 14:29:06 -0500

    fix service, fix abstract sertvice

--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/ICashRegisterService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/ICashRegisterService.java
@@ -2,7 +2,13 @@ package com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_service
 
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.CashRegisterRequest;
 import com.riwi.localstorage.riwi_local_storage.api.dto.response.CashRegisterResponse;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.CreateService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.ReadAllService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.ReadService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.UpdateService;
 
-public interface ICashRegisterService extends CrudAbstractSevice<CashRegisterRequest, CashRegisterResponse, String> {
-  
+public interface ICashRegisterService extends CreateService<CashRegisterRequest, CashRegisterResponse>,
+      ReadService<CashRegisterResponse, String>, 
+      UpdateService<CashRegisterRequest, CashRegisterResponse, String>,
+      ReadAllService<CashRegisterResponse> {
 }
--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/CashRegisterService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/CashRegisterService.java
@@ -1,8 +1,10 @@
 package com.riwi.localstorage.riwi_local_storage.infrastructure.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,16 +29,18 @@ public class CashRegisterService implements ICashRegisterService {
   private CashRegisterMapper cashRegisterMapper;
 
   @Override
-  public CashRegisterResponse getById(String id) {
+  public Optional<CashRegisterResponse> getById(String id) {
 
-    return cashRegisterMapper.cashRegisterToCashRegisterResponsesponse(this.find(id));
+    Optional<CashRegister> supplier = cashRegisterRepository.findById(id);
+
+    return supplier.map(cashRegisterMapper::cashRegisterToCashRegisterResponse);
   }
 
   @Override
   public CashRegisterResponse create(CashRegisterRequest request) {
     CashRegister cashRegister = cashRegisterMapper.cashRegisterRequestToCashRegister(request);
 
-    return cashRegisterMapper.cashRegisterToCashRegisterResponsesponse(this.cashRegisterRepository.save(cashRegister));
+    return cashRegisterMapper.cashRegisterToCashRegisterResponse(this.cashRegisterRepository.save(cashRegister));
   }
 
   @Override
@@ -44,27 +48,13 @@ public class CashRegisterService implements ICashRegisterService {
     CashRegister cashRegister = this.find(id);
     cashRegister = cashRegisterMapper.cashRegisterRequestToCashRegister(request);
     cashRegister.setId(id);
-    return cashRegisterMapper.cashRegisterToCashRegisterResponsesponse(this.cashRegisterRepository.save(cashRegister));
+    return cashRegisterMapper.cashRegisterToCashRegisterResponse(this.cashRegisterRepository.save(cashRegister));
   }
 
   @Override
-  public void delete(String id) {
-    this.cashRegisterRepository.delete(this.find(id));
-  }
-
-  @Override
-  public Page<CashRegisterResponse> getAll(int page, int size) {
-    if (page < 0)
-      page = 0;
-
-    PageRequest pagination = PageRequest.of(page, size);
-
-    this.cashRegisterRepository.findAll(pagination);
-
-    return this.cashRegisterRepository.findAll(pagination)
-        .map(cashRegisterMapper::cashRegisterToCashRegisterResponsesponse);
-
-  }
+  public Page<CashRegisterResponse> getAll(Pageable pageable) {
+    return cashRegisterRepository.findAll(pageable).map(cashRegisterMapper::cashRegisterToCashRegisterResponse);
+}
 
   private CashRegister find(String id) {
     // this method is incomplete, missing idNotFoud
