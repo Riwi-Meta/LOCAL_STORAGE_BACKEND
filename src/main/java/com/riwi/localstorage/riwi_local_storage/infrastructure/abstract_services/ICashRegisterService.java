1320e0d293173d5582da248f09b10ed558063c39
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-06-26 09:38:09 -0500

    added CashRegisterMapper

--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/ICashRegisterService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/ICashRegisterService.java
@@ -1,8 +1,7 @@
 package com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services;
 
-import java.util.UUID;
-
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.CashRegisterRequest;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.CashRegisterResponse;
 
 public interface ICashRegisterService extends CrudAbstractSevice<CashRegisterRequest, CashRegisterResponse, String> {
   
