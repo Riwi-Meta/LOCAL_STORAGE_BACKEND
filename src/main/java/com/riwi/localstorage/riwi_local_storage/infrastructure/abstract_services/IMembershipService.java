26dfc76f2a2cb877fa91943d16bb43a779dbee27
EstebanBedoyaHiguita <estebanbedoyahiguita@gmail.com>
2024-06-26 17:07:46 -0500

    Read_a_Membership with service and controller ok

--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/IMembershipService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/IMembershipService.java
@@ -2,12 +2,15 @@ package com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_service
 
 import org.springframework.data.domain.Page;
 
-import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.MembershipRequest;
+
 import com.riwi.localstorage.riwi_local_storage.api.dto.response.MembershipResponse;
 
-public interface IMembershipService {
+
+public interface IMembershipService{
 
     public Page<MembershipResponse> getAll(int page, int size);
 
     public void updateMembershipStatus(String id, boolean enabled);
+
+    public MembershipResponse getById(String id);
 }
