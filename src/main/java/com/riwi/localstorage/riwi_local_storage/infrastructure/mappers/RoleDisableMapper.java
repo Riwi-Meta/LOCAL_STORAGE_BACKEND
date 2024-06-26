9ebcebcbe7bc56f463106ac9fde98d6f8e3d73c9
Ana Valentina Marín Usuga <anav.usuga@gmail.com>
2024-06-26 11:26:42 -0500

    Create Mappers of Role Entity

---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/mappers/RoleDisableMapper.java
@@ -0,0 +1,22 @@
+package com.riwi.localstorage.riwi_local_storage.infrastructure.mappers;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.update.RoleRequestUpdateStatus;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.RoleResponse;
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Role;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface RoleDisableMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "description", ignore = true)
+    Role toEntity(RoleRequestUpdateStatus request);
+
+    @InheritInverseConfiguration
+    RoleResponse toResponse(Role entity);
+
+}
---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/mappers/RoleUpdateMapper.java
@@ -0,0 +1,21 @@
+package com.riwi.localstorage.riwi_local_storage.infrastructure.mappers;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.update.RoleRequestUpdate;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.RoleResponse;
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Role;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface RoleUpdateMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    Role toEntity(RoleRequestUpdate request);
+
+    @InheritInverseConfiguration
+    RoleResponse toResponse(Role entity);
+    
+}
