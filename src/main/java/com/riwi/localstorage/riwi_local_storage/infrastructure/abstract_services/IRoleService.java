958c55f4b8f4d1e7ea5a37b8d4712ecdb8645cf9
juansefdz <jsfm81091@gmail.com>
2024-06-26 12:50:45 -0500

    update basic structure whit new changes

--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/IRoleService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/IRoleService.java
@@ -1,8 +1,19 @@
 package com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services;
 
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.RoleRequest;
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.update.RoleRequestUpdate;
 import com.riwi.localstorage.riwi_local_storage.api.dto.response.RoleResponse;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.CreateService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.DeleteService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.ReadAllService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.ReadService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.UpdateService;
 
-public interface IRoleService  extends CRUDService<RoleRequest, RoleResponse, Long>  {
+public interface IRoleService extends 
+    CreateService<RoleRequest,RoleResponse>,
+    DeleteService<Long>,                                //this is disable status
+    ReadAllService<RoleResponse>,
+    ReadService<RoleResponse,Long>,
+    UpdateService<RoleRequestUpdate,RoleResponse,Long> {
     
 }
