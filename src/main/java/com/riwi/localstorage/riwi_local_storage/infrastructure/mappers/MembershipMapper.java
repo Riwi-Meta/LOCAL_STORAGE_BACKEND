ede3d6af3ad245fbe79f112af5c7bfadc59a239f
SMoreno73 <sebastianmoreno301177@correo.itm.edu.co>
2024-06-30 15:43:01 -0500

    simple update

--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/mappers/MembershipMapper.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/mappers/MembershipMapper.java
@@ -1,8 +1,6 @@
 package com.riwi.localstorage.riwi_local_storage.infrastructure.mappers;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
+import org.mapstruct.*;
 
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.MembershipRequest;
 import com.riwi.localstorage.riwi_local_storage.api.dto.response.MembershipResponse;
@@ -11,11 +9,20 @@ import com.riwi.localstorage.riwi_local_storage.domain.entities.Membership;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MembershipMapper {
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "user", ignore = true)
-  @Mapping(target = "enabled", ignore = true)
-  Membership requestToEntity(MembershipRequest membershipRequest);
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "user", ignore = true),
+            @Mapping(target = "enabled", ignore = true)
+    })
+    Membership requestToEntity(MembershipRequest membershipRequest);
 
-  MembershipResponse entityToResponse(Membership membership);
+    MembershipResponse entityToResponse(Membership membership);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "user", ignore = true),
+            @Mapping(target = "enabled", ignore = true)
+    })
+    Membership updateEntity(MembershipRequest membershipRequest, @MappingTarget Membership membership);
 
 }
