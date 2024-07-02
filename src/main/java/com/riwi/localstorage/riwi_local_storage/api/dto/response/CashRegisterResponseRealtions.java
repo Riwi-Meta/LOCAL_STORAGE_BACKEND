98b924cf86b3ad80645ecd106c0eda01dd0981eb
danielsm <danielsanchezm92@gmail.com>
2024-07-02 06:51:59 -0500

    Dto response extends from CashRegisterResponse

---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/api/dto/response/CashRegisterResponseRealtions.java
@@ -0,0 +1,17 @@
+package com.riwi.localstorage.riwi_local_storage.api.dto.response;
+
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Cash;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CashRegisterResponseRealtions extends CashRegisterResponse{
+    private Cash cash;
+    //Corregir el response cuando se junte
+}
