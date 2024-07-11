76f58e0cc826c4918c4aa643590182fceade1824
dannyKentala <handres41@outlook.com>
2024-07-11 08:24:35 -0500

    finalizado los mappers junto con los dtos

--- src/main/java/com/riwi/localstorage/riwi_local_storage/api/dto/request/update/StoreUpdateDisableRequest.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/api/dto/request/update/StoreUpdateDisableRequest.java
@@ -1,5 +1,16 @@
 package com.riwi.localstorage.riwi_local_storage.api.dto.request.update;
 
+import com.riwi.localstorage.riwi_local_storage.util.enums.StatusType;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreUpdateDisableRequest {
-    
+
+    @NotNull(message = "Status is required")
+    private StatusType status;
+
 }
