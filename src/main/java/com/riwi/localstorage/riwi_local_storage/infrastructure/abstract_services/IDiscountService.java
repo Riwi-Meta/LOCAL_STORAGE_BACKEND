9a51329179ac8db5cb6d48639bf3504378907060
SantiagoProgramador <andresx1277@gmail.com>
2024-07-11 06:54:14 -0500

    Complete read discount operations

---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/abstract_services/IDiscountService.java
@@ -0,0 +1,19 @@
+package com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.DiscountRequest;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.DiscountResponse;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.CreateService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.DeleteService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.ReadAllService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.ReadService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.generic.UpdateService;
+
+public interface IDiscountService extends CreateService<DiscountRequest, DiscountResponse>, DeleteService<String>,
+    ReadAllService<DiscountResponse>, ReadService<DiscountResponse, String>,
+    UpdateService<DiscountRequest, DiscountResponse, String> {
+
+    public Page<DiscountResponse>  findAllByIsActive(boolean isActive,Pageable pageable);
+}
---
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/DiscountService.java
@@ -0,0 +1,71 @@
+package com.riwi.localstorage.riwi_local_storage.infrastructure.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.DiscountRequest;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.DiscountResponse;
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Discount;
+import com.riwi.localstorage.riwi_local_storage.domain.repositories.DiscountRepository;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.IDiscountService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.mappers.DiscountMapper;
+import com.riwi.localstorage.riwi_local_storage.util.exeptions.IdNotFoundException;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class DiscountService implements IDiscountService {
+
+  @Autowired
+  private final DiscountRepository discountRepository;
+
+  @Autowired
+  private final DiscountMapper discountMapper;
+
+  @Override
+  public DiscountResponse create(DiscountRequest request) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'create'");
+  }
+
+  @Override
+  public void delete(String id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  }
+
+  @Override
+  public Page<DiscountResponse> getAll(Pageable pageable) {
+    return this.discountRepository.findAll(pageable).map(this.discountMapper::discountToDiscountResponse);
+  }
+
+  @Override
+  public Optional<DiscountResponse> getById(String id) {
+    Discount discount = findDiscount(id);
+
+    return Optional.ofNullable(this.discountMapper.discountToDiscountResponse(discount));
+  }
+
+  public Discount findDiscount(String id) {
+    return discountRepository.findById(id).orElseThrow(() -> new IdNotFoundException("Discount ", id));
+  }
+
+  @Override
+  public DiscountResponse update(String id, DiscountRequest request) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'update'");
+  }
+
+  @Override
+  public Page<DiscountResponse> findAllByIsActive(boolean isActive, Pageable pageable) {
+    return this.discountRepository.findAllByIsActive(isActive, pageable)
+        .map(this.discountMapper::discountToDiscountResponse);
+
+  }
+
+}
