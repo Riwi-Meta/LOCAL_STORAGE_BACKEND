a5e9e350503d4044a17fe7bfc9ac19773f709b25
Juan Camilo <jccaste1002@gmail.com>
2024-07-12 07:32:30 -0500

    create service and controller update

--- src/main/java/com/riwi/localstorage/riwi_local_storage/api/controllers/StoreController.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/api/controllers/StoreController.java
@@ -1,14 +1,29 @@
 package com.riwi.localstorage.riwi_local_storage.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.StoreRequest;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.StoreResponse;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.IStoreService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "stores")
 @AllArgsConstructor
 public class StoreController {
+
+    @Autowired
+    private final IStoreService service;
 
     /*----------------------
      * GET ALL ROLES
@@ -28,8 +43,20 @@ public class StoreController {
      * CREATE ROLE
      * -------------------
      */
-
-     //insert here your code
+        @Operation(summary = "creates a new store", description = "create a new store by entering the required data")
+        @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "SUCCESSFUL"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "401", description = "NOT AUTHORIZED"),
+            @ApiResponse(responseCode = "403", description = "FORBIDDEN ACCESS"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        })
+        @PostMapping
+        public ResponseEntity<StoreResponse>create(
+            @Validated @RequestBody StoreRequest request
+        ){
+            return ResponseEntity.ok(this.service.create(request));
+        }
 
     /*----------------------
      * UPDATE ROLE
--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/StoreService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/StoreService.java
@@ -2,6 +2,7 @@ package com.riwi.localstorage.riwi_local_storage.infrastructure.services;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -9,16 +10,36 @@ import org.springframework.stereotype.Service;
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.StoreRequest;
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.update.StoreRequestUpdate;
 import com.riwi.localstorage.riwi_local_storage.api.dto.response.StoreResponse;
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Store;
+import com.riwi.localstorage.riwi_local_storage.domain.repositories.StoreRepository;
 import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.IStoreService;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.mappers.StoreMapper;
+import com.riwi.localstorage.riwi_local_storage.util.exeptions.StoreNameAlreadyExistsException;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Service
+@Data
 @AllArgsConstructor
-public class StoreService implements IStoreService {@Override
+public class StoreService implements IStoreService {
+    
+    @Autowired
+    private final StoreRepository storeRepository;
+
+    @Autowired
+    private final StoreMapper storeMapper;
+    
+    @Override
     public StoreResponse create(StoreRequest request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        String name = request.getName();
+        Optional<Store> existingStoreName = this.storeRepository.findByName(name);
+        if (existingStoreName.isPresent()) {
+            throw new StoreNameAlreadyExistsException("Store with name "+name+" already exists");
+        }else{
+            Store store = this.storeMapper.toEntity(request);
+            return this.storeMapper.toResponse(this.storeRepository.save(store));
+        }
     }
 
     @Override
