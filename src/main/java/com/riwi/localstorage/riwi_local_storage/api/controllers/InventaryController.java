869587fc7b965ce290ecafee1cfd050f5edcd4df
valentinaduarte <valentinadc1@hotmail.com>
2024-07-17 08:44:19 -0500

    Updating Inventory service and controller with create microservice

--- src/main/java/com/riwi/localstorage/riwi_local_storage/api/controllers/InventaryController.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/api/controllers/InventaryController.java
@@ -1,7 +1,15 @@
 package com.riwi.localstorage.riwi_local_storage.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.InventaryRequest;
+import com.riwi.localstorage.riwi_local_storage.api.dto.response.InventaryResponse;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.IInventaryService;
 
 import lombok.AllArgsConstructor;
 
@@ -9,40 +17,45 @@ import lombok.AllArgsConstructor;
 @RequestMapping(path = "inventaries")
 @AllArgsConstructor
 public class InventaryController {
+    @Autowired
+    private final IInventaryService service;
 
     /*----------------------
      * GET ALL INVENTARIES
      * ---------------------
      */
 
-     //insert here your code
+    // insert here your code
 
-     /*--------------------
-     * GET BY ID
-     * -------------------
-     */
+    /*--------------------
+    * GET BY ID
+    * -------------------
+    */
 
-     //insert here your code
+    // insert here your code
 
     /*--------------------
      * CREATE INVENTARY
      * -------------------
      */
 
-     //insert here your code
+    public ResponseEntity<InventaryResponse> create(
+            @Validated @RequestBody InventaryRequest request) {
+        return ResponseEntity.ok(this.service.create(request));
+    }
 
     /*----------------------
      * UPDATE INVENTARY
      * ---------------------
      */
 
-     //insert here your code
+    // insert here your code
 
     /*----------------------
      * DELETE INVENTARY (DISABLE ROLE - SOFT DELETE)
      * ---------------------
      */
 
-     //insert here your code
-    
+    // insert here your code
+
 }
