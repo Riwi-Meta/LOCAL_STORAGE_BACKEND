e200376d6d7cc05fdd80174954b0b0d67a977c8d
miguelangito <miguelangelespinalospina@gmail.com>
2024-07-19 08:49:04 -0500

    update service started

--- src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/InventoryService.java
+++ src/main/java/com/riwi/localstorage/riwi_local_storage/infrastructure/services/InventoryService.java
@@ -3,12 +3,6 @@ package com.riwi.localstorage.riwi_local_storage.infrastructure.services;
 import java.util.Date;
 import java.util.Optional;
 
-import com.riwi.localstorage.riwi_local_storage.domain.entities.Branch;
-import com.riwi.localstorage.riwi_local_storage.domain.entities.Product;
-import com.riwi.localstorage.riwi_local_storage.domain.repositories.BranchRepository;
-import com.riwi.localstorage.riwi_local_storage.domain.repositories.ProductRepository;
-import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.IInventoryService;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,75 +11,97 @@ import org.springframework.stereotype.Service;
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.create.InventoryRequest;
 import com.riwi.localstorage.riwi_local_storage.api.dto.request.update.InventoryRequestUpdate;
 import com.riwi.localstorage.riwi_local_storage.api.dto.response.InventoryResponse;
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Branch;
 import com.riwi.localstorage.riwi_local_storage.domain.entities.Inventory;
+import com.riwi.localstorage.riwi_local_storage.domain.entities.Product;
+import com.riwi.localstorage.riwi_local_storage.domain.repositories.BranchRepository;
 import com.riwi.localstorage.riwi_local_storage.domain.repositories.InventoryRepository;
+import com.riwi.localstorage.riwi_local_storage.domain.repositories.ProductRepository;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.abstract_services.IInventoryService;
 import com.riwi.localstorage.riwi_local_storage.infrastructure.mappers.inventoryMappers.InventoryCreateMapper;
 import com.riwi.localstorage.riwi_local_storage.infrastructure.mappers.inventoryMappers.InventoryMapper;
+import com.riwi.localstorage.riwi_local_storage.infrastructure.mappers.inventoryMappers.InventoryUpdateMapper;
 import com.riwi.localstorage.riwi_local_storage.util.exeptions.IdNotFoundException;
 import com.riwi.localstorage.riwi_local_storage.util.exeptions.InventoryExpirationDatePassed;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class InventoryService implements IInventoryService {
-    
-    @Autowired
-    private final InventoryMapper inventoryMapper;
+	
+	@Autowired
+	private final InventoryMapper inventoryMapper;
 
-    @Autowired
-    private final InventoryCreateMapper createMapper;
-       
-    @Autowired
-    private final InventoryRepository inventoryRepository;
+	@Autowired
+	private final InventoryCreateMapper createMapper;
+	
+	@Autowired
+	private final InventoryRepository inventoryRepository;
 
-    @Autowired
-    private final ProductRepository productRepository;
-    @Autowired
-    private final BranchRepository branchRepository;
+	@Autowired
+	private final ProductRepository productRepository;
 
-    @Override
-    public InventoryResponse create(InventoryRequest request) {
-        Date today = new Date();
+	@Autowired
+	private final BranchRepository branchRepository;
 
-        if (request.getExpirationDate().before(today)) {
-            throw new InventoryExpirationDatePassed("The expiration date of the item in the inventory has passed");
-        }
+	@Autowired
+	private final InventoryUpdateMapper updateMapper;
+
+	@Override
+	public InventoryResponse create(InventoryRequest request) {
+		Date today = new Date();
+
+		if (request.getExpirationDate().before(today)) {
+			throw new InventoryExpirationDatePassed("The expiration date of the item in the inventory has passed");
+		}
 
 
-        Product product = productRepository.findById(request.getProductId())
-                .orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + request.getProductId()));
+		Product product = productRepository.findById(request.getProductId())
+				.orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + request.getProductId()));
 
-        Branch branch = branchRepository.findById(request.getBranchId())
-                .orElseThrow(() -> new EntityNotFoundException("Branch not found with id: " + request.getBranchId()));
+		Branch branch = branchRepository.findById(request.getBranchId())
+				.orElseThrow(() -> new EntityNotFoundException("Branch not found with id: " + request.getBranchId()));
 
-        Inventory inventory = createMapper.toEntity(request);
-        inventory.setProduct(product);
-        inventory.setBranch(branch);
+		Inventory inventory = createMapper.toEntity(request);
+		inventory.setProduct(product);
+		inventory.setBranch(branch);
 
-        Inventory savedInventory = inventoryRepository.save(inventory);
+		Inventory savedInventory = inventoryRepository.save(inventory);
 
-        return createMapper.toResponse(savedInventory);
-    }
-    @Override
-    public InventoryResponse update(String id, InventoryRequestUpdate request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
-    }
+		return createMapper.toResponse(savedInventory);
+	}
+	@Override
+	public InventoryResponse update(String id, InventoryRequestUpdate request) {
+		
+		Inventory inventory = this.find(id);
 
-    
-    @Override
-    public Page<InventoryResponse> getAll(Pageable pageable) {
-        return this.inventoryRepository.findAll(pageable).map(this.inventoryMapper::toResponse);
-    }
+		Inventory toUpdate= this.updateMapper.toEntity(request);
+		
+		toUpdate.setId(inventory.getId());
+		toUpdate.setLastUpdateDate(new Date());
 
-    @Override
-    public Optional<InventoryResponse> getById(String id) {
-        return Optional.ofNullable(this.inventoryMapper.toResponse(this.find(id)));
-    }
+		if (toUpdate.getQuantity() < 0 || toUpdate.getQuantity() <= inventory.getQuantity()) {
+			return null;
+		}
 
-    private Inventory find(String id){
-        return this.inventoryRepository.findById(id).orElseThrow(()->new IdNotFoundException("Inventory", id));
-    }
+		return this.updateMapper.toResponse(this.inventoryRepository.save(toUpdate));
+	}
+
+	
+	@Override
+	public Page<InventoryResponse> getAll(Pageable pageable) {
+		return this.inventoryRepository.findAll(pageable).map(this.inventoryMapper::toResponse);
+	}
+
+	@Override
+	public Optional<InventoryResponse> getById(String id) {
+		return Optional.ofNullable(this.inventoryMapper.toResponse(this.find(id)));
+	}
+
+	private Inventory find(String id){
+		return this.inventoryRepository.findById(id).orElseThrow(()->new IdNotFoundException("Inventory", id));
+	}
 
 }
